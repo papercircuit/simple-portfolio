6c33566d7be10df28f7701333d8ef96111e572fc
Kenny Johnson <105308787+papercircuit@users.noreply.github.com>
2023-03-07 10:30:21 -0500

    meta data added, head removed from App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
 import Portfolio from "./Portfolio";
 import Background from "./Background";
 import FaviconView from "./FaviconView";
 import Favicon from "./Favicon";
 
-
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-
+<BrowserRouter>
+  <App />
+</BrowserRouter>
 
 function App() {
   return (
     <div>
-      <head>
-        <Favicon />
-      </head>
+      <Favicon />
       <Routes>
         <Route exact path="/" element={<Portfolio />} />
         <Route exact path="/background" element={<Background />} />
