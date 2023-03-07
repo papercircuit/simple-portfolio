b263ee8ad00922bca4433e8eb4f25ce28def1990
Kenny Johnson <kenny.johnson.nyc@gmail.com>
2023-03-06 19:11:47 -0500

    Favicon set to random shapes

--- src/Favicon.js
+++ src/Favicon.js
@@ -7,15 +7,23 @@ const Favicon = () => {
     canvas.height = 16;
     const ctx = canvas.getContext("2d");
 
-    // Generate a random pattern for the favicon
-    for (let i = 0; i < 16; i++) {
-      for (let j = 0; j < 16; j++) {
-        ctx.fillStyle = `rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(
-          Math.random() * 256
-        )}, ${Math.floor(Math.random() * 256)}, 1)`;
-        ctx.fillRect(i, j, 1, 1);
-      }
+    // Generate a random shape for the favicon
+    ctx.beginPath();
+    ctx.moveTo(Math.random() * 16, Math.random() * 16);
+    for (let i = 0; i < 5; i++) {
+      ctx.arc(
+        Math.random() * 16,
+        Math.random() * 16,
+        Math.random() * 8,
+        0,
+        Math.PI * 2,
+        false
+      );
     }
+    ctx.fillStyle = `rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(
+      Math.random() * 256
+    )}, ${Math.floor(Math.random() * 256)}, 1)`;
+    ctx.fill();
 
     // Set the favicon link tag in the head of the document
     const favicon = document.querySelector('link[rel="icon"]');
