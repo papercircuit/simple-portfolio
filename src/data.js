9764e03ddd034ab80e10c8d939b67dbf2328da2a
Kenny Johnson <kenny.johnson.nyc@gmail.com>
2023-03-18 11:55:15 -0400

    portoflio project added, repo sort cleaned

--- src/data.js
+++ src/data.js
@@ -1,12 +1,13 @@
 import satelliteVisualization from "./assets/gifs/satellites.gif";
 import leaves from "./assets/gifs/leaves.gif";
+import portfolio from "./assets/portfolio.png";
 
 export const data = [
   {
     id: 1,
-    name: "Satellite Visualization",
+    name: "Satellite Visualizer",
     description:
-      "Built with Vanilla JS, Highcharts 3D, HTML, and CSS. This project uses the NASA API to display satellite data in a 3D environment.",
+      "Built with Vanilla JS, Highcharts.js, HTML, and CSS. This project uses the NASA API to display satellite location data in a 3D environment.",
     img: satelliteVisualization,
     link: "https://github.com/papercircuit/DSCOVR-ACE-Visualizer",
   },
@@ -16,7 +17,7 @@ export const data = [
     description:
       "Built with React, HTML, and CSS. This project uses React Hooks to increment a counter for example and (hopefully) educational purposes.",
     img: "https://github.com/papercircuit/react-hooks-counter-examples/blob/master/public/screenshot.png?raw=true",
-    link: "https://github.com/papercircuit/increment-counter-using-react-hooks"
+    link: "https://github.com/papercircuit/increment-counter-using-react-hooks",
   },
   {
     id: 2,
@@ -30,8 +31,16 @@ export const data = [
     id: 3,
     name: "Worm's Eye View",
     description:
-      "Uses React, Firebase, and Restful to create give the user a location and time of year specific selection of plants that will grow in their area.",
+      "Uses React, Firebase, and multiple Restful APIs to give the user a location and time specific, selection of plants that will grow in their area.",
     img: "https://github.com/team-mind-capstone/worms-eye-view/blob/master/readmeAssets/home_plant_sug.jpg?raw=true",
     link: "https://github.com/team-mind-capstone/worms-eye-view",
-  }
+  },
+  {
+    id: 4,
+    name: "This Portfolio",
+    description:
+      "Built with React, Framer Motion, and Tailwind CSS. This portfolio is a work in progress and will be updated as I learn more.",
+    img: portfolio,
+    link: "https://github.com/papercircuit/simple-portfolio",
+  },
 ];
