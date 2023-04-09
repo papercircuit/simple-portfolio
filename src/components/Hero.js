b57d5118b200e7c90bf3a116495c044e2762bef5
Kenny Johnson <kenny.johnson.nyc@gmail.com>
2023-04-09 11:13:06 -0400

    Hero padding changed to margin top

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -30,7 +30,7 @@ const Hero = () => {
     <div className="h-[50vh] md:min-h-[90vh] px-10 relative flex flex-col justify-between">
       <div className="my-auto relative pr-4 border-r-0 md:border-r-4 border-gray-700 dark:border-gray-300">
         <motion.h1
-          className="text-4xl pt-2 md:text-6xl font-bold text-gray-900 dark:text-white leading-tight"
+          className="text-4xl mt-2 md:text-6xl font-bold text-gray-900 dark:text-white leading-tight"
           animate={{ opacity: 1 }}
           transition={{
             duration: 0.2,
